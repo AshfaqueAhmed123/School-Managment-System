01e0fdf5b9ba22dc506f54e14f32592b6d00e0c6
AshfaqueAhmed123 <ashfaqueahmed2342@gmail.com>
2024-11-22 17:08:09 +0500

    add basic nav bar and a demo logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import Navbar from './components/custom/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <Button className="m-3 text-xl">Click me</Button>
+    <div className='min-w-[100vw] min-h-[100vh] h-[max] bg-slate-900 text-white'>
+      <Navbar/>
     </div>
   )
 }
