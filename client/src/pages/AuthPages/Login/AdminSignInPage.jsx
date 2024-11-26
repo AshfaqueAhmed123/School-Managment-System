96d8141c658a767b4ab7577200dc46ad8d9e7001
AshfaqueAhmed123 <ashfaqueahmed2342@gmail.com>
2024-11-26 17:29:44 +0500

    add login functionality to parent student admin and teacher in the client side

--- client/src/pages/AuthPages/Login/AdminSignInPage.jsx
+++ client/src/pages/AuthPages/Login/AdminSignInPage.jsx
@@ -1,5 +1,11 @@
 import * as React from "react"
-import {Link} from "react-router-dom"
+import {useState} from "react"
+import {Link,useNavigate} from "react-router-dom"
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const notify = (message="something went wrong") => toast.error(message);
 
 import { Button } from "@/components/ui/button"
 import {
@@ -12,17 +18,41 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
 
 const AdminSignInPage = () => {
+
+  let navigate = useNavigate();
+    
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
+    
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      (async ()=>{
+        let res = await fetch("http://localhost:4000/admin/login",{
+          method:"POST",
+          headers:{
+            "content-type":"application/json",
+          },
+          body:JSON.stringify({
+            email,password
+          })
+        });
+        res = await res.json()
+        console.log(res);
+        if(res.statusCode == 200){
+          const {accessToken,refreshToken} = res?.data
+          localStorage.setItem("adminToken",accessToken);
+          navigate("/admin")
+        } else{
+          notify(res?.message || "something went wrong")
+        }
+      })(); 
+    }
+
   return (
     <div className="bg-[#383854] w-full h-screen flex flex-col items-center justify-center gap-4 text-white">
+       <ToastContainer />
         <h1 className="text-3xl font-sans mt-[-100px] mb-5 text-center">login to your Admin dashboard</h1>
     <Card className="w-[350px] bg-[#383854] text-white border-2 border-white">
       <CardHeader>
@@ -30,15 +60,19 @@ const AdminSignInPage = () => {
         <CardDescription>sign in to your Admin dashboard.</CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="email" className="text-xl">Email</Label>
-              <Input id="email" placeholder="email@exmaple.com" />
+              <Input value={email} onChange={(e)=>{
+                setEmail(e.target.value)
+              }} id="email" placeholder="email@exmaple.com" />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="password" className="text-xl">Password</Label>
-              <Input id="password" placeholder="*******" />
+              <Input type="password" value={password} onChange={(e)=>{
+                setPassword(e.target.value)
+              }} id="password" placeholder="*******" />
             </div>
           </div>
         </form>
@@ -47,7 +81,7 @@ const AdminSignInPage = () => {
         <Link to="/login">
         <Button variant="outline" className="bg-white text-black capitalize">Cancel</Button>
         </Link>
-        <Button className="capitalize">sign in</Button>
+        <Button className="capitalize" type="submit" onClick={handleSubmit}>sign in</Button>
       </CardFooter>
     </Card>
     </div>
